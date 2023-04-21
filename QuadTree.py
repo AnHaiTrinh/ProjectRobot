93dbbcb8e40d18aaed77ffc5cab777c90585ab3b
hieu <mhieu.vn@gmail.com>
2023-04-21 15:44:35 +0700

    Add controller

--- QuadTree.py
+++ QuadTree.py
@@ -33,6 +33,8 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 WHITE = (255, 255, 255)
+
+#Frequency of global replan
 PATIENCE = 30
 
 # Initialization
@@ -142,7 +144,7 @@ while not finished:
             if (len(past_path) == 0) or robot.pos != past_path[-1]:
                 past_path.append(robot.pos)
             decision = robot.decisionMaking(obstacles_list_before, obstacles_list_after, local_goal)
-            print(decision)
+            # print(decision)
             if decision == "Replan":
                 path = robot.updatePath(obstacles_list, priority_queue, env)
                 # print("Changed")
@@ -181,8 +183,11 @@ while not finished:
             if patience == PATIENCE:
                 patience = 0
         else:
-            # env = QuadTreeEnvironment(LEFT_PAD + env_width / 2, NORTH_PAD + env_height / 2, env_width, env_height)
-            env = GridEnvironment(LEFT_PAD + env_width / 2, NORTH_PAD + env_height / 2, env_width, env_height)
+            #Quadtree
+            env = QuadTreeEnvironment(LEFT_PAD + env_width / 2, NORTH_PAD + env_height / 2, env_width, env_height)
+
+            #Grid
+            # env = GridEnvironment(LEFT_PAD + env_width / 2, NORTH_PAD + env_height / 2, env_width, env_height)
             env.update(obstacles_list)
             env.build_env(robot.pos, end)
             priority_queue = SortedList(key=lambda x: x.key)
@@ -223,7 +228,7 @@ def angle(p1, p2, p3):
     return np.pi - np.arccos(a)
 
 
-with open("metrics", "a") as f:
+with open("comparison", "a") as f:
     d = 0
     for i in range(1, len(past_path)):
         d += np.sqrt(np.sum(np.square(np.array(past_path[i-1]) - np.array(past_path[i]))))
@@ -232,5 +237,6 @@ with open("metrics", "a") as f:
         count += 1
         smooth += angle(past_path[i - 1], past_path[i], past_path[i + 1])
     # Write distance, smoothness and time of an execution to an output file
-    f.write(map + ' ' + str(round(d, 4)) + ' ' + str(round((smooth / count) * 180 / np.pi, 4)) +
-            ' ' + str(round(end_time - start_time, 4)) + '\n')
+    # f.write(map + ' ' + str(round(d, 4)) + ' ' + str(round((smooth / count) * 180 / np.pi, 4)) +
+    #         ' ' + str(round(end_time - start_time, 4)) + '\n')
+    f.write(f"{map}: {round(d, 4)} {round((smooth / count) * 180 / np.pi, 4)} {round(end_time - start_time, 4)} \n")
