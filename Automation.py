80fee2b606f570a9158ba3f11bd13547468bcd19
DESKTOP-R201FAL\Thinkpad <trinhanhai1710@gmail.com>
2023-05-04 15:18:05 +0700

    add rooms

--- Automation.py
+++ Automation.py
@@ -2,5 +2,5 @@ from QuadTree import main
 
 scenario = input("Enter scenario: ")
 algorithm = input("Enter algorithm: ")
-for i in range(1, 11):
+for i in range(1, 21):
     main(algorithm, scenario, scenario + str(i), False)
