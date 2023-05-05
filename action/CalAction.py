65fa32b7dc3eee4ce4e60e741fe6294f22b9fb41
hieu <mhieu.vn@gmail.com>
2023-05-05 15:33:53 +0700

    fix

--- action/CalAction.py
+++ action/CalAction.py
@@ -4,9 +4,9 @@ pd.set_option('display.max_colwidth', None)
 pd.set_option('display.max_rows', None)
 
 scenarios = ['dense', 'maze', 'room', 'trap']
-scenario = 'trap'
+scenario = input("Scenario: ")
 algorithms = os.listdir(scenario)
 for algorithm in algorithms:
     df = pd.read_csv(scenario + '/' + algorithm, sep=' ', names=['Map', 'Action', 'Time'])
     res = df.groupby(['Map', 'Action']).mean()
-    res.to_excel('output.xlsx', sheet_name=scenario)
+    res.to_excel(scenario + '_output.xlsx', sheet_name=scenario)
